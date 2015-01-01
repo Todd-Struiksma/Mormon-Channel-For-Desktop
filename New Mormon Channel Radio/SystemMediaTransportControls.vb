1e90def1660a98feeaa64c7afaddb72f977c13bc
Todd-Struiksma <toddler619@gmail.com>
2015-01-01 03:08:10 -0800

    coo

---
+++ New Mormon Channel Radio/SystemMediaTransportControls.vb
@@ -0,0 +1,5 @@
+﻿Public NotInheritable Class SystemMediaTransportControls
+    Public Property IsPlayEnabled As Boolean
+    Public Property IsPauseEnabled As Boolean
+    Public Event ButtonPressed As TypedEventHandler(Of SystemMediaTransportControls, SystemMediaTransportControlsButtonPressedEventArgs)
+End Class
---
+++ New Mormon Channel Radio/SystemMediaTransportControlsButtonPressedEventArgs.vb
@@ -0,0 +1,3 @@
+﻿Public Class SystemMediaTransportControlsButtonPressedEventArgs
+
+End Class
