22e0b21597a8860e9553c7eba8d774bfcb4cf142
Todd-Struiksma <toddler619@gmail.com>
2015-01-01 03:08:10 -0800

    Initial Commit

---
+++ New Mormon Channel Radio/MainPage.xaml.vb
@@ -0,0 +1,60 @@
+﻿
+
+' The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+'Declaration
+
+
+
+Public NotInheritable Class MainPage
+    Inherits Page
+
+
+
+
+
+
+    Private Sub AppBarButton_Click(sender As Object, e As RoutedEventArgs)
+        MediaElement.Source = (New Uri("http://d1jwofs8l1nyio.cloudfront.net/new_mormon"))
+
+    End Sub
+
+    Private Sub MediaElement_MediaOpened(sender As Object, e As RoutedEventArgs)
+
+        '  Slider.Value = MediaElement.Volume * 100
+    End Sub
+
+
+    Private Sub AppBarButton_Click_1(sender As Object, e As RoutedEventArgs)
+        MediaElement.Source = (New Uri("http://d1jwofs8l1nyio.cloudfront.net/new_music"))
+    End Sub
+
+
+
+    Private Sub AppBarButton_Click_2(sender As Object, e As RoutedEventArgs)
+        MediaElement.Source = (New Uri("http://d1jwofs8l1nyio.cloudfront.net/spanish"))
+    End Sub
+
+    Private Sub AppBarButton_Click_3(sender As Object, e As RoutedEventArgs)
+
+        MediaElement.Source = (New Uri("http://d1jwofs8l1nyio.cloudfront.net/motab"))
+
+
+
+    End Sub
+
+
+    Private Sub Slider_ValueChanged_1(sender As Object, e As RangeBaseValueChangedEventArgs) Handles Slider.ValueChanged
+        MediaElement.Volume = Slider.Value.ToString / 100
+    End Sub
+
+    Private Sub MediaElement_MediaOpened_1(sender As Object, e As RoutedEventArgs) Handles MediaElement.MediaOpened
+        MediaElement.Volume = Slider.Value.ToString / 100
+        ' Slider.Value = MediaElement.Volume.ToString * 100
+    End Sub
+
+
+    Private Sub MainPage_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+    End Sub
+End Class
+
---
+++ New Mormon Channel Radio/MainPageBackup.xaml.vb
@@ -0,0 +1,47 @@
+﻿' The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+'Declaration
+
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private Sub AppBarButton_Click(sender As Object, e As RoutedEventArgs)
+        MediaElement.Source = (New Uri("http://d1jwofs8l1nyio.cloudfront.net/new_mormon"))
+
+    End Sub
+
+    Private Sub MediaElement_MediaOpened(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+
+        Frame.Height = "50"
+
+        MediaElement.Volume = 0.5
+    End Sub
+
+    Private Sub AppBarButton_Click_1(sender As Object, e As RoutedEventArgs)
+        MediaElement.Source = (New Uri("http://d1jwofs8l1nyio.cloudfront.net/new_music"))
+    End Sub
+
+ 
+
+    Private Sub AppBarButton_Click_2(sender As Object, e As RoutedEventArgs)
+        MediaElement.Source = (New Uri("http://d1jwofs8l1nyio.cloudfront.net/spanish"))
+    End Sub
+
+    Private Sub AppBarButton_Click_3(sender As Object, e As RoutedEventArgs)
+        MediaElement.Source = (New Uri("http://d1jwofs8l1nyio.cloudfront.net/motab"))
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Slider_ValueChanged(sender As Object, e As RangeBaseValueChangedEventArgs)
+        MediaElement.Volume = Slider.Value.ToString / 100
+    End Sub
+
+  
+End Class
